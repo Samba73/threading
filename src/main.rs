ebe4aacefa811b180b9d5e96db1d5cf57e265d6c
Samba <sambasivam.k@gmail.com>
2024-02-11 20:30:55 +0530

    added join to finish spawned thread

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::thread;
 use std::time::Duration;
 fn main() {
-  thread::spawn(|| {
-    for i in 1 .. 10 {
+  let handle = thread::spawn(|| {
+    for i in 1 .. 50 {
         println!("The number {} is from spawned thread", i,);
         thread::sleep(Duration::from_millis(1));   
     }
@@ -12,4 +12,6 @@ fn main() {
     println!("The number {} is from main thread", i);
     thread::sleep(Duration::from_millis(1));
   }
+
+  handle.join().unwrap();
 }
