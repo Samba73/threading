693383bb59582c60f6f51118fd1c6344b1e2fa47
Samba <sambasivam.k@gmail.com>
2024-02-11 20:23:40 +0530

    threading first

---
+++ target/debug/deps/threading.d
@@ -0,0 +1,5 @@
+C:\Users\AW954XQ\rust\threading\target\debug\deps\threading.exe: src\main.rs
+
+C:\Users\AW954XQ\rust\threading\target\debug\deps\threading.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/threading.d
@@ -0,0 +1,1 @@
+C:\Users\AW954XQ\rust\threading\target\debug\threading.exe: C:\Users\AW954XQ\rust\threading\src\main.rs
